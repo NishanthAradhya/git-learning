52301cd9464d911c0c3585be0451c521cb59cd07
nishanth aradhya <nishanth.aradhya@gmail.com>
2023-11-02 14:03:10 +0100

    added

--- gitlearning/src/App.js
+++ gitlearning/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Native
+          Learn Native react
         </a>
       </header>
     </div>
